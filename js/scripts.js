ff2d53ae09a147b8fdabb68d3cc002c62b3dd2ec
Johnny Jachna <johnnyjachna@gmail.com>
2025-05-15 20:09:43 -0400

    Complete exercise

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
-'use strict';
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("DOM Content Loaded");
+
+    const generateListButton = document.querySelector("#generateList");
+    const myList = document.querySelector("#myList");
+
+    generateListButton.addEventListener("click", function () {
+        //alert('CLICKED');
+        //myList.innerHTML = "It's Alive!!!!";
+
+        const inputItems = document.querySelectorAll("input");
+        const listElement = document.createElement("ul");
+
+        inputItems.forEach(function(inputItem) {
+            const listItem = document.createElement("li");
+            
+            listItem.innerText = inputItem.value;
+            listElement.appendChild(listItem);
+        }); 
+        
+        myList.appendChild(listElement);    
+    });
+}); 
